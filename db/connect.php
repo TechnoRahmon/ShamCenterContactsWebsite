e592eee650a730de9a76991305ef25f09768a867
Lordslomer <slome.lord123@gmail.com>
2019-03-25 00:40:52 +0100

    Update connect.php

--- db/connect.php
+++ db/connect.php
@@ -1,9 +1,9 @@
 <?php
     
-    $dbServername = "ec2-54-246-92-116.eu-west-1.compute.amazonaws.com";
-    $dbUsername = "jyiwxtfraxtnjx";
-    $dbPassword = "5327bef564362147ad24f6e317c0bac765a1cf23e473ea4de2ad0b2f60314f7f";
-    $dbName = "d924vchjd5ti4j";
+    $dbServername = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbName = "shamcentercontactsso";
 
     $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
     
