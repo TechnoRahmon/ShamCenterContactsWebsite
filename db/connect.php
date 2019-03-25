3f3ee6f02d714058b60f925ffc6439fd4ed15487
SalamSki <48717734+lordslomer@users.noreply.github.com>
2019-03-25 21:11:13 +0100

    Update connect.php

--- db/connect.php
+++ db/connect.php
@@ -1,9 +1,9 @@
 <?php
     
-    $dbServername = "localhost";
-    $dbUsername = "root";
-    $dbPassword = "";
-    $dbName = "shamcentercontactsso";
+    $dbServername = "Server=MYSQL6001.site4now.net";
+    $dbUsername = "a46d51_sccdata";
+    $dbPassword = "SLOME123karo123";
+    $dbName = "db_a46d51_sccdata";
 
     $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
     
