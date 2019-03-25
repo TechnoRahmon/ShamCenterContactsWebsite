dc805cb6fe083c366ddbb43763710c95b28f086c
SalamSki <48717734+lordslomer@users.noreply.github.com>
2019-03-25 21:57:50 +0100

    Add files via upload

--- db/connect.php
+++ db/connect.php
@@ -1,6 +1,6 @@
 <?php
     
-    $dbServername = "Server=MYSQL6001.site4now.net";
+    $dbServername = "mysql6001.site4now.net";
     $dbUsername = "a46d51_sccdata";
     $dbPassword = "SLOME123karo123";
     $dbName = "db_a46d51_sccdata";
